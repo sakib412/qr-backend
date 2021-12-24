40fbdb4eff63c7515aa66c399e750b801d1ecc37
Najmus Sakib <nazmusakib412@gmail.com>
2021-12-24 15:59:58 +0600

    [FIXED] cloudinary setting 2

--- backend/settings.py
+++ backend/settings.py
@@ -28,13 +28,13 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage',
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
     "corsheaders",
     'attendence',
      # Media Cloudinary
+    'cloudinary_storage',
     'cloudinary',
     
 ]
