d4357c7ff4b0c9b40750e0a80af41ceccbaba521
Najmus Sakib <nazmusakib412@gmail.com>
2021-12-24 15:51:01 +0600

    debug true

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", default="SECreT_kEy") or "SECreT_kEy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["qr-attendance-backend.herokuapp.com", "localhost"]
 
